f3ebf10ae59cb46b168946d6d43e1837d80b6ac6
zachrolfness <zach.rolfness@gmail.com>
2017-06-27 00:28:45 -0700

    Main repo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD_PUnWIvKuhpvZZ9CmLV68CmH1Cr64t4w',
+    authDomain: 'firstpathways-5b7e3.firebaseapp.com',
+    databaseURL: 'https://firstpathways-5b7e3.firebaseio.com',
+    projectId: 'firstpathways-5b7e3',
+    storageBucket: 'firstpathways-5b7e3.appspot.com',
+    messagingSenderId: '332717744424'
+  }
 };
