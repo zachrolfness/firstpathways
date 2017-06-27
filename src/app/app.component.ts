75ba1a6cd0af865324b273a919675df8a1b7eb48
Kesav Kadalazhi <kkadalazhi1@pvlearners.net>
2017-06-27 02:23:38 -0700

    Updated Bootstrap. Added Home, About, Start and Travel Pages. Added Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  
+
   items: FirebaseListObservable<any[]>;
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items');
